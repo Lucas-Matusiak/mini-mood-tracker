7f31d1d34f3c61b81bd5351fd8993f94fe72f68c
Lucas-Matusiak <lucassmatusiak@gmail.com>
2025-07-21 20:39:00 +0200

    delete tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
+import './index.css';
 
-
-function App() {
-   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Tailwind + React + TypeScript!
-      </h1>
+export default function App() {
+  return (
+    <div className="">
+      <h1 className="">Salut le monde !</h1>
     </div>
   );
 }
-
-export default App
