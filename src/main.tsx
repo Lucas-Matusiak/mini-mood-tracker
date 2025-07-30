b529422652e698df205ee5875770b14f7d47192d
Lucas-Matusiak <lucassmatusiak@gmail.com>
2025-07-30 12:29:53 +0200

    new design calendar

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./main.css";
 import { MoodApp } from "./components/MoodApp/MoodApp.tsx";
 
 createRoot(document.getElementById("root")!).render(
