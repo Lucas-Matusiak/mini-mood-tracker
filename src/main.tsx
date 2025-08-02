038a7ec918ff2a30315315867bce75e6e6f5ae5b
Lucas-Matusiak <lucassmatusiak@gmail.com>
2025-08-02 19:04:08 +0200

    nouvelle classe style.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
 import { MoodApp } from "./components/MoodApp/MoodApp.tsx";
-
+import "../styles/styles.css"
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MoodApp />
